ffe1de50ae2ca6f36e247fe47d3d039d3e2089d7
DavidS12321 <83694683+DavidS12321@users.noreply.github.com>
2024-05-13 19:00:24 -0700

    Delete Customer.java

--- Customer.java
+++
@@ -1,9 +0,0 @@
-class Customer {
-	private String name=null;
-	private String address=null;
-	private String county=null; 
-	private String dietaryRestriction=null; // should be an enum?
-	public Customer(String name) {
-		this.name=name;
-	}
-}
