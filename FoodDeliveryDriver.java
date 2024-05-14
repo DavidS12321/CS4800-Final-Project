b206dad85f9b4c9b3081b8fd941ffbfabc5a51ce
DavidS12321 <83694683+DavidS12321@users.noreply.github.com>
2024-05-13 18:59:51 -0700

    Delete FoodDeliveryDriver.java

--- FoodDeliveryDriver.java
+++
@@ -1,9 +0,0 @@
-class FoodDeliveryDriver {
-	public static void main(String[] args) {
-		FoodDeliveryApp cppFoodDelivery = new FoodDeliveryApp();
-		cppFoodDelivery.register(new Customer("Alice"));
-		Restaurant restaurant = new Restaurant("Bob's");
-		restaurant.add("Burger");
-		cppFoodDelivery.register(restaurant);
-	}
-}
