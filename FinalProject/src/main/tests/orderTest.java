aabb298fe3ed1c9bf395a57b46a9697417516f56
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-05-15 01:04:25 -0700

    added more to test

--- FinalProject/src/main/tests/orderTest.java
+++ FinalProject/src/main/tests/orderTest.java
@@ -1,14 +1,24 @@
+import java.util.Random;
+
 import org.junit.Test;
 
 public class orderTest {
-    Restaurant testRestaurant = new Restaurant("Testaurant", "Test Address", "LA County", null, null, null, null);
-    DietaryRestriction testDR = DietaryRestriction.NO_RESTRICTION;
-    Customer testCustomer = new Customer("Testomer", "Fake House Ave", "LA County", testDR);
-    Order testOrder = new Order(testRestaurant, testCustomer, null);
+    
     
     @Test 
     public void testPlaceOrder()
     {
+        Restaurant testRestaurant = new Restaurant("Testaurant", "Test Address", "LA County", "08:00 - 22:00", null, null, null);
+        DietaryRestriction testDR = DietaryRestriction.NO_RESTRICTION;
+        Customer testCustomer = new Customer("Testomer", "Fake House Ave", "LA County", testDR);
+        Shift testShift = Shift.FIRST_SHIFT;
+        Driver testDriver = new Driver("Testriver", "Fake Apartment Ave", "LA County", testShift);
+        Order testOrder = new Order(testRestaurant, testCustomer, testDriver);
 
+        boolean testVariable = testOrder.placeOrder();
+        boolean expected = true;
+        assertEquals(expected, testVariable);
     }
+
+    
 }
