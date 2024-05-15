081ff77ae9990918bd854938bbca8cd5385d88ea
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-05-15 14:09:11 -0700

    Made decorator files menuItem, Topping, and beans

--- FinalProject/src/main/java/Topping.java
+++ FinalProject/src/main/java/Topping.java
@@ -1,3 +1,16 @@
-public class Topping {
+public class Topping implements menuItem{
+    public String name;
+
+    public Topping(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String displayItem()
+    {
+       return name;
+    }
+
     
 }
