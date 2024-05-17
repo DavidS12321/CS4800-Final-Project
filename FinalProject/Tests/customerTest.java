c068ee7fcd14f5a61740757f614b461872fe9441
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-05-17 13:05:25 -0700

    Moved Files and made 2 more tests, customerTest and userFactoryTest

---
+++ FinalProject/Tests/customerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class customerTest {
+
+    DietaryRestriction NoRestriction = DietaryRestriction.NO_RESTRICTION;
+    Customer customer = new Customer("Test Customer", "Random House", "LA County", NoRestriction);
+
+ 
+    @Test 
+    public void TestGetDietaryRestriction()
+    {
+        DietaryRestriction test = customer.getDietaryRestriction();
+        assertEquals(NoRestriction, test);
+    }
+    
+}
