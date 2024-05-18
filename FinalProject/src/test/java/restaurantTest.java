f4082158cd8ece07d70f4ca0ed26e367ce930674
DavidS12321 <dshahi333@gmail.com>
2024-05-17 20:48:20 -0700

    MealChecker, Meal, UserFactory, and Driver tests all done.

--- FinalProject/src/test/java/restaurantTest.java
+++ FinalProject/src/test/java/restaurantTest.java
@@ -6,7 +6,7 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class RestaurantTest {
+public class restaurantTest {
 
 
     @Test
